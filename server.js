d55de7a4d0f776097f583c44fddfb60bc0c8c42f
basilzuberi <basilzuberi@hotmail.com>
2020-03-18 16:18:01 -0400

    changes for hamdan

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-var express = require('express'), 
+var express = require('express'),
     app = express(),
     http = require('http'),
     socketio = require('socket.io');
 
-var server =  http.createServer(app);
+var server = http.createServer(app);
 var io = socketio.listen(server);
 // start webserver on port 8080
 server.listen(8080);
 // add the public directory where we have our index
 app.use(express.static(__dirname + '/public'));
 console.log("Server running on port 8080");
+
+let users = [];
+
+io.on('connection', (socket) => {
+
+});
